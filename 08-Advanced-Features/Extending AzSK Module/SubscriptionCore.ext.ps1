61b8b266a220fc062bc3b48741907be298e22caf
sapsh-msft <v-sapsh@microsoft.com>
2018-11-29 22:37:15 +0530

    Added reference files for extending AzSK module

---
+++ 08-Advanced-Features/Extending AzSK Module/SubscriptionCore.ext.ps1
@@ -0,0 +1,17 @@
+Set-StrictMode -Version Latest
+
+# Class name must be SubscriptionCoreExt. Class must be inherited from SubscriptionCore class
+class SubscriptionCoreExt: SubscriptionCore
+{
+    SubscriptionCoreExt([string] $subscriptionId):
+    Base($subscriptionId)
+    {       
+        
+    }
+
+    hidden [ControlResult] FunctionToExtend([ControlResult] $controlResult)
+    {
+        # Your function logic goes here.
+        return $controlResult;
+    }
+}
