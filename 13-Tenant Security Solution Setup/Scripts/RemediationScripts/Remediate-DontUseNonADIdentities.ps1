d1b47a1aac4f4bfc163c21e21d896ad7b1fbd3e9
Rohit Yadav <v-royad@microsoft.com>
2021-04-13 22:45:41 +0530

    Adding remediation and roll back script for 'Azure_Subscription_AuthZ_Dont_Use_NonAD_Identities' control

---
+++ 13-Tenant Security Solution Setup/Scripts/RemediationScripts/Remediate-DontUseNonADIdentities.ps1
@@ -0,0 +1,331 @@
+function Pre_requisites
+{
+    <#
+    .SYNOPSIS
+    This command would check pre requisities modules.
+    .DESCRIPTION
+    This command would check pre requisities modules to perform remediation.
+	#>
+
+    Write-Host "Required modules are: Az.Resources, Az.Account" -ForegroundColor Cyan
+    Write-Host "Checking for required modules..."
+    $availableModules = $(Get-Module -ListAvailable Az.Resources, AzureAD, Az.Accounts)
+    
+    # Checking if 'Az.Accounts' module is available or not.
+    if($availableModules.Name -notcontains 'Az.Accounts')
+    {
+        Write-Host "Installing module Az.Accounts..." -ForegroundColor Yellow
+        Install-Module -Name Az.Accounts -Scope CurrentUser
+    }
+    else
+    {
+        Write-Host "Az.Resources module is available." -ForegroundColor Green
+    }
+
+    # Checking if 'Az.Resources' module is available or not.
+    if($availableModules.Name -notcontains 'Az.Resources')
+    {
+        Write-Host "Installing module Az.Resources..." -ForegroundColor Yellow
+        Install-Module -Name Az.Resources -Scope CurrentUser
+    }
+    else
+    {
+        Write-Host "Az.Resources module is available." -ForegroundColor Green
+    }
+}
+
+function Remove-NonAADAccountsRBAC
+{
+    <#
+    .SYNOPSIS
+    This command would help in remediating 'Azure_Storage_AuthN_Dont_Allow_Anonymous' control.
+    .DESCRIPTION
+    This command would help in remediating 'Azure_Storage_AuthN_Dont_Allow_Anonymous' control.
+    .PARAMETER SubscriptionId
+        Enter subscription id on which remediation need to perform.
+    .PARAMETER ObjectIds
+        Enter objectIds of non ad identities.
+    .Parameter Force
+        Enter force parameter value to remove non ad identities
+    .PARAMETER PerformPreReqCheck
+        Perform pre requisities check to ensure all required module to perform roll back operation is available.
+    #>
+
+    param (
+        [string]
+        $SubscriptionId,
+
+        [string[]]
+        $ObjectIds,
+
+        [switch]
+        $Force,
+
+        [switch]
+        $PerformPreReqCheck
+    )
+
+    Write-Host "======================================================"
+    Write-Host "Starting with removal of Non AAD Identities from subscriptions..."
+    Write-Host "------------------------------------------------------"
+
+    if($PerformPreReqCheck)
+    {
+        try 
+        {
+            Write-Host "Checking for pre-requisites..."
+            Pre_requisites
+            Write-Host "------------------------------------------------------"  
+        }
+        catch 
+        {
+            Write-Host "Error occured while checking pre-requisites. ErrorMessage [$($_)]" -ForegroundColor $([Constants]::MessageType.Error)    
+            break
+        }
+    }
+
+    # Connect to AzAccount
+    $isContextSet = Get-AzContext
+    if ([string]::IsNullOrEmpty($isContextSet))
+    {       
+        Write-Host "Connecting to AzAccount..."
+        Connect-AzAccount
+        Write-Host "Connected to AzAccount" -ForegroundColor Green
+    }
+
+    # Setting context for current subscription.
+    $currentSub = Set-AzContext -SubscriptionId $SubscriptionId
+
+
+    Write-Host "Metadata Details: `n SubscriptionId: [$($SubscriptionId)] `n AccountName: [$($currentSub.Account.Id)] `n AccountType: [$($currentSub.Account.Type)]"
+    Write-Host "------------------------------------------------------"
+    Write-Host "Starting with Subscription [$($SubscriptionId)]..."
+
+
+    Write-Host "Step 1 of 3: Validating whether the current user [$($currentSub.Account.Id)] have the required permissions to run the script for Subscription [$($SubscriptionId)]..."
+
+    # Safe Check: Checking whether the current account is of type User and also grant the current user as UAA for the sub to support fallback
+    if($currentSub.Account.Type -ne "User")
+    {
+        Write-Host "Warning: This script can only be run by user account type." -ForegroundColor Yellow
+        break;
+    }
+
+    # Safe Check: Current user need to be either UAA or Owner for the subscription
+    $currentLoginRoleAssignments = Get-AzRoleAssignment -SignInName $currentSub.Account.Id -Scope "/subscriptions/$($SubscriptionId)";
+
+    if(($currentLoginRoleAssignments | Where { $_.RoleDefinitionName -eq "Owner"  -or $_.RoleDefinitionName -eq 'CoAdministrator' -or $_.RoleDefinitionName -eq "User Access Administrator" } | Measure-Object).Count -le 0)
+    {
+        Write-Host "Warning: This script can only be run by an Owner or User Access Administrator" -ForegroundColor Yellow
+        break;
+    }
+    
+    Write-Host "Step 2 of 3: Fetching all the role assignments for Subscription [$($SubscriptionId)]..."
+
+    #  Getting all role assignments (ARM, Classic) of subscription.
+    $currentRoleAssignmentList = Get-AzRoleAssignment -IncludeClassicAdministrators  
+    # filter-out classic admins
+    $currentRoleAssignmentList = $currentRoleAssignmentList | where {$_.ObjectId -ne [Guid]::Empty};
+    $distinctRoleAssignmentList = @();
+
+    # adding one valid role assignment, so that even if graph call works, it has to get atleast 1. If we dont get any, means Graph API failed.
+    $distinctRoleAssignmentList += $currentLoginRoleAssignments;
+
+    # Getting role assignment and filtering service principal object type
+    if(($ObjectIds | Measure-Object).Count -eq 0)
+    {
+        $distinctRoleAssignmentList += $currentRoleAssignmentList | Where-Object { ![string]::IsNullOrWhiteSpace($_.SignInName) }
+    }
+    else
+    {
+        $ObjectIds | Foreach-Object {
+          $objectId = $_;
+           if(![string]::IsNullOrWhiteSpace($objectId))
+            {
+                $distinctRoleAssignmentList += Get-AzRoleAssignment -ObjectId $objectId | Where-Object { ![string]::IsNullOrWhiteSpace($_.SignInName) }
+            }
+            else
+            {
+                Write-Host "Warning: Dont pass empty string array in the ObjectIds param. If you dont want to use the param, just remove while executing the command" -ForegroundColor Yellow
+                break;
+            }  
+        }
+    }
+   
+    # Defining regex to filter Non AAD Identities 
+    $NonADIdentitiesPatterns = @( "(.)*#ext(.)*" )
+
+    # Defining regex to filter allowed Non AAD Identities
+    $ApprovedNonADIndentitiesPatterns = @( "(.)*_sas.msft.net#ext#@microsoft.onmicrosoft.com" )
+
+    # Creating regex to filter Non AAD Account
+    $NonADIdentitiesPattern = (('^' + (($NonADIdentitiesPatterns | foreach {[regex]::escape($_)}) -join '|') + '$')) -replace '[\\]',''
+
+    # Filtering Non AAD Identities
+    $liveAccounts = [array]($distinctRoleAssignmentList | Where-Object {$_.SignInName -and $_.SignInName.ToLower() -imatch $NonADIdentitiesPattern} )
+
+    #Exclude whitelisted patterns for non AAD identities
+    if( ($liveAccounts | Measure-Object).Count -gt 0 -and ($ApprovedNonADIndentitiesPatterns | Measure-Object).Count -ne 0)
+    {
+        $ApprovedNonADIndentitiesPattern = (('^' + (($ApprovedNonADIndentitiesPatterns | foreach {[regex]::escape($_)}) -join '|') + '$')) -replace '[\\]',''
+        $liveAccounts = [array]($liveAccounts | Where-Object {$_.SignInName -and $_.SignInName.ToLower() -inotmatch $ApprovedNonADIndentitiesPattern} )
+    }	
+
+    # Safe Check: Check whether the current user accountId is part of Invalid AAD ObjectGuids List 
+    if(($liveAccounts | where { $_.ObjectId -eq $currentLoginRoleAssignments.ObjectId} | Measure-Object).Count -gt 0)
+    {
+        Write-Host "Warning: Current User account is found as part of the Non AAD Account. This is not expected behaviour. This can happen typically during Graph API failures. Aborting the operation. Reach out to aztssup@microsoft.com" -ForegroundColor Yellow
+        break;
+    }
+
+    if(($liveAccounts | Measure-Object).Count -le 0)
+    {
+        Write-Host "No Non AAD accounts found for the subscription [$($SubscriptionId)]. Exiting the process." -ForegroundColor Cyan
+        break;
+    }
+    else
+    {
+        Write-Host "Found [$(($liveAccounts | Measure-Object).Count)] Non AAD role assingments for the subscription [$($SubscriptionId)]" -ForegroundColor Cyan
+    }
+
+    $folderPath = [Environment]::GetFolderPath("MyDocuments") 
+    if (Test-Path -Path $folderPath)
+    {
+        $folderPath += "\AzTS\Remediation\Subscriptions\$($subscriptionid.replace("-","_"))\$((Get-Date).ToString('yyyyMMdd_hhmm'))\NonAADAccounts\"
+        New-Item -ItemType Directory -Path $folderPath | Out-Null
+    }
+
+    # Safe Check: Taking backup of Non AAD identities    
+    if ($liveAccounts.length -gt 0)
+    {
+        Write-Host "Taking backup of Non AAD Accounts role assignments that needs to be removed. Please do not delete this file. Without this file you wont be able to rollback any changes done through Remediation script." -ForegroundColor Cyan
+        $liveAccounts | ConvertTo-json -Depth 10 | out-file "$($folderpath)\NonAADAccountsRoleAssignments.json"       
+    }
+
+    if(-not $Force)
+    {
+        Write-Host "Do you want to delete the above listed role assignment? " -ForegroundColor Yellow -NoNewline
+        $UserInput = Read-Host -Prompt "(Y|N)"
+
+        if($UserInput -ne "Y")
+        {
+            break;
+        }
+    }
+   
+
+    Write-Host "Step 3 of 3: Clean up Non AAD Accounts for Subscription [$($SubscriptionId)]..."
+    
+    # Start deletion of all Non AAD Accounts.
+    Write-Host "Starting to delete Non AAD Accounts role assignments..." -ForegroundColor Cyan
+    $liveAccounts | Remove-AzRoleAssignment -Verbose
+    Write-Host "Completed deleting Non AAD Accounts role assignments." -ForegroundColor Green    
+}
+
+
+function Restore-AzTSInvalidAADAccounts
+{
+    <#
+    .SYNOPSIS
+    This command would help in performing roll back operation for 'Azure_Storage_AuthN_Dont_Allow_Anonymous' control.
+    .DESCRIPTION
+    This command would help in performing roll back operation for 'Azure_Storage_AuthN_Dont_Allow_Anonymous' control.
+    .PARAMETER SubscriptionId
+        Enter subscription id on which roll back operation need to perform.
+    .PARAMETER RollbackFilePath
+        Json file path which containing remediation log to perform roll back operation.
+    .PARAMETER PerformPreReqCheck
+        Perform pre requisities check to ensure all required module to perform roll back operation is available.
+	#>
+
+    param (
+        [string]
+        $SubscriptionId,       
+
+        [string]
+        $RollbackFilePath,
+        
+        [switch]
+        $PerformPreReqCheck
+    )
+
+    Write-Host "======================================================"
+    Write-Host "Starting with restore invalid AAD object guids from subscriptions..."
+    Write-Host "------------------------------------------------------"
+    
+    if($PerformPreReqCheck)
+    {
+        try 
+        {
+            Write-Host "Checking for pre-requisites..."
+            Pre_requisites
+            Write-Host $([Constants]::SingleDashLine)    
+        }
+        catch 
+        {
+            Write-Host "Error occured while checking pre-requisites. ErrorMessage [$($_)]" -ForegroundColor $([Constants]::MessageType.Error)    
+            break
+        }    
+    }
+
+    $isContextSet = Get-AzContext
+    if ([string]::IsNullOrEmpty($isContextSet))
+    {       
+        Write-Host "Connecting to AzAccount..."
+        Connect-AzAccount
+        Write-Host "Connected to AzAccount" -ForegroundColor Green
+    }
+
+    # Setting context for current subscription.
+    $currentSub = Set-AzContext -SubscriptionId $SubscriptionId
+
+    Write-Host "Metadata Details: `n SubscriptionId: [$($SubscriptionId)] `n AccountName: [$($currentSub.Account.Id)] `n AccountType: [$($currentSub.Account.Type)]"
+    Write-Host "------------------------------------------------------"
+    Write-Host "Starting with Subscription [$($SubscriptionId)]..."
+
+
+    Write-Host "Step 1 of 3: Validating whether the current user [$($currentSub.Account.Id)] have the required permissions to run the script for Subscription [$($SubscriptionId)]..."
+
+    # Safe Check: Checking whether the current account is of type User and also grant the current user as UAA for the sub to support fallback
+    if($currentSub.Account.Type -ne "User")
+    {
+        Write-Host "Warning: This script can only be run by user account type." -ForegroundColor Yellow
+        break;
+    }
+
+    # Safe Check: Current user need to be either UAA or Owner for the subscription
+    $currentLoginRoleAssignments = Get-AzRoleAssignment -SignInName $currentSub.Account.Id -Scope "/subscriptions/$($SubscriptionId)";
+
+    if(($currentLoginRoleAssignments | Where { $_.RoleDefinitionName -eq "Owner"  -or $_.RoleDefinitionName -eq 'CoAdministrator' -or $_.RoleDefinitionName -eq "User Access Administrator" } | Measure-Object).Count -le 0)
+    {
+        Write-Host "Warning: This script can only be run by an Owner or User Access Administrator" -ForegroundColor Yellow
+        break;
+    }
+
+    Write-Host "Step 2 of 3: Check for presence of rollback file for Subscription: [$($SubscriptionId)]..."
+
+    if (-not (Test-Path -Path $RollbackFilePath))
+    {
+        Write-Host "Warning: Rollback file is not found. Please check if the initial Remediation script has been run from the same machine. Exiting the process" -ForegroundColor Yellow
+        break;        
+    }
+    $backedUpRoleAssingments = Get-Content -Raw -Path $RollbackFilePath | ConvertFrom-Json     
+
+    Write-Host "Step 3 of 3: Restore role assingments [$($SubscriptionId)]..."
+    $backedUpRoleAssingments | ForEach-Object {
+        $roleAssignment = $_;
+        New-AzRoleAssignment -ObjectId $roleAssignment.ObjectId -Scope $roleAssignment.Scope -RoleDefinitionName $roleAssignment.RoleDefinitionName -ErrorAction SilentlyContinue | Out-Null;
+    }    
+    Write-Host "Completed restoring role assignments." -ForegroundColor Green
+}
+
+# ***************************************************** #
+<#
+Function calling with parameters.
+Remove-AzTSInvalidAADAccounts -SubscriptionId '<Sub_Id>' -ObjectIds @('<Object_Ids>')  -Force:$false -PerformPreReqCheck: $true
+
+Function to roll back role assignments as per input remediated log
+Restore-AzTSInvalidAADAccounts -SubscriptionId '<Sub_Id>' -RollbackFilePath "<user Documents>\AzTS\Remediation\Subscriptions\<subscriptionId>\<JobDate>\InvalidAADAccounts\InvalidRoleAssignments.json"
+
+Note: You can only rollback valid role assignments.
+#>
